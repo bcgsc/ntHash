8611738ede0e69721593931c2f63820a937141ad
Ben Vandervalk <benv@bcgsc.ca>
2016-04-15 14:37:30 -0700

    rolling.h: add setBase() functions
    These functions change a single base within a k-mer and update the
    hash values accordingly.

--- rolling.h
+++ rolling.h
@@ -114,6 +114,26 @@ inline uint64_t rollHashesLeft(uint64_t& fhVal, uint64_t& rhVal, const unsigned 
     return (rhVal<fhVal)? rhVal : fhVal;
 }
 
+// change a single base and update forward-strand hash value accordingly
+inline uint64_t setBase(uint64_t fhVal, char* kmerSeq, unsigned pos, char base, unsigned k)
+{
+    fhVal ^= rol(seedTab[(unsigned char)kmerSeq[pos]], k-1-pos);
+	kmerSeq[pos] = base;
+    fhVal ^= rol(seedTab[(unsigned char)kmerSeq[pos]], k-1-pos);
+	return fhVal;
+}
+
+// change a single base and update hash values accordingly
+inline uint64_t setBase(uint64_t& fhVal, uint64_t& rhVal, char* kmerSeq, unsigned pos, char base, unsigned k)
+{
+    fhVal ^= rol(seedTab[(unsigned char)kmerSeq[pos]], k-1-pos);
+	rhVal ^= rol(seedTab[(unsigned char)kmerSeq[pos]+cpOff], pos);
+	kmerSeq[pos] = base;
+    fhVal ^= rol(seedTab[(unsigned char)kmerSeq[pos]], k-1-pos);
+	rhVal ^= rol(seedTab[(unsigned char)kmerSeq[pos]+cpOff], pos);
+    return (rhVal<fhVal)? rhVal : fhVal;
+}
+
 // spaced-seed hash values
 
 /**
@@ -244,6 +264,31 @@ inline uint64_t rollHashesLeft(uint64_t &fkVal, uint64_t &rkVal, const char * se
         if(seedSeq[i]!='1') {
             fsVal ^= rol(seedTab[(unsigned char)kmerSeq[i-1]], k-1-i);
             rsVal ^= rol(seedTab[(unsigned char)kmerSeq[i-1]+cpOff], i);
+		}
+    }
+    return (rsVal<fsVal)? rsVal : fsVal;
+}
+
+/**
+ * Change a single base and recompute spaced seed hash values
+ *
+ * @param fkVal hash value for current k-mer unmasked and in forward orientation
+ * @param rkVal hash value for current k-mer unmasked and in reverse complement orientation
+ * @param seedSeq bitmask indicating "don't care" positions for hashing
+ * @param kmerSeq sequence for current k-mer
+ * @param pos position of base to change
+ * @param base new base value
+ * @param k k-mer size
+ * @return updated canonical hash value for masked k-mer
+ */
+inline uint64_t setBase(uint64_t& fkVal, uint64_t& rkVal, const char * seedSeq, char * kmerSeq, unsigned pos, char base, unsigned k)
+{
+	setBase(fkVal, rkVal, kmerSeq, pos, base, k);
+	uint64_t fsVal=fkVal, rsVal=rkVal;
+    for(unsigned i=0; i<k; i++) {
+        if(seedSeq[i]!='1') {
+            fsVal ^= rol(seedTab[(unsigned char)kmerSeq[i]], k-1-i);
+            rsVal ^= rol(seedTab[(unsigned char)kmerSeq[i]+cpOff], i);
 		}
     }
     return (rsVal<fsVal)? rsVal : fsVal;
