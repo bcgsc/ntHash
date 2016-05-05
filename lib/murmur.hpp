8b564f3273cd201cc347a46ca75d42b34c043fe8
Hamid Mohamadi <mohamadi.hamid@gmail.com>
2016-05-04 18:45:38 -0700

    Update murmur.hpp

--- lib/murmur.hpp
+++ lib/murmur.hpp
@@ -3,6 +3,14 @@
 
 #include <stdint.h>
 
+//-----------------------------------------------------------------------------
+// MurmurHash2, 64-bit versions, by Austin Appleby
+
+// The same caveats as 32-bit MurmurHash2 apply here - beware of alignment 
+// and endian-ness issues if used across multiple platforms.
+
+// 64-bit hash for 64-bit platforms
+
 uint64_t MurmurHash64A ( const void * key, int len, unsigned int seed ) {
     const uint64_t m = 0xc6a4a7935bd1e995;
     const int r = 47;
