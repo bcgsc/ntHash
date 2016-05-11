f2dc883a87907f6a18f2df32e69d6981e89f0a9d
mohamadi <mohamadi.hamid@gmail.com>
2016-05-11 13:01:18 -0700

    Add canonical multi-hash functions

--- nthash.hpp
+++ nthash.hpp
@@ -6,7 +6,6 @@
  * British Columbia Cancer Agency
  */
 
-
 #ifndef NT_HASH_H
 #define NT_HASH_H
 
@@ -21,11 +20,12 @@ const int multiShift = 27;
 // seed for gerenerating multiple hash values
 static const uint64_t multiSeed = 0x90b45d39fb6da1fa;
 
-// 64-bit random seed table corresponding to bases and their complements
+// 64-bit random seeds corresponding to bases and their complements
 static const uint64_t seedA = 0x3c8bfbb395c60474;
 static const uint64_t seedC = 0x3193c18562a02b4c;
 static const uint64_t seedG = 0x20323ed082572324;
 static const uint64_t seedT = 0x295549f54be24456;
+static const uint64_t seedN = 0x0000000000000000;
 
 static const uint64_t vecA[64] = {
     0x3c8bfbb395c60474,0x7917f7672b8c08e8,0xf22feece571811d0,0xe45fdd9cae3023a1,0xc8bfbb395c604743,0x917f7672b8c08e87,0x22feece571811d0f,0x45fdd9cae3023a1e,
@@ -33,31 +33,31 @@ static const uint64_t vecA[64] = {
     0xfbb395c604743c8b,0xf7672b8c08e87917,0xeece571811d0f22f,0xdd9cae3023a1e45f,0xbb395c604743c8bf,0x7672b8c08e87917f,0xece571811d0f22fe,0xd9cae3023a1e45fd,
     0xb395c604743c8bfb,0x672b8c08e87917f7,0xce571811d0f22fee,0x9cae3023a1e45fdd,0x395c604743c8bfbb,0x72b8c08e87917f76,0xe571811d0f22feec,0xcae3023a1e45fdd9,
     0x95c604743c8bfbb3,0x2b8c08e87917f767,0x571811d0f22feece,0xae3023a1e45fdd9c,0x5c604743c8bfbb39,0xb8c08e87917f7672,0x71811d0f22feece5,0xe3023a1e45fdd9ca,
-    0xc604743c8bfbb395,0x8c08e87917f7672b,0x1811d0f22feece57,0x3023a1e45fdd9cae,0x604743c8bfbb395c,0xc08e87917f7672b8,0x811d0f22feece571,0x23a1e45fdd9cae3,
-    0x4743c8bfbb395c6,0x8e87917f7672b8c,0x11d0f22feece5718,0x23a1e45fdd9cae30,0x4743c8bfbb395c60,0x8e87917f7672b8c0,0x1d0f22feece57181,0x3a1e45fdd9cae302,
-    0x743c8bfbb395c604,0xe87917f7672b8c08,0xd0f22feece571811,0xa1e45fdd9cae3023,0x43c8bfbb395c6047,0x87917f7672b8c08e,0xf22feece571811d,0x1e45fdd9cae3023a
+    0xc604743c8bfbb395,0x8c08e87917f7672b,0x1811d0f22feece57,0x3023a1e45fdd9cae,0x604743c8bfbb395c,0xc08e87917f7672b8,0x811d0f22feece571,0x023a1e45fdd9cae3,
+    0x04743c8bfbb395c6,0x08e87917f7672b8c,0x11d0f22feece5718,0x23a1e45fdd9cae30,0x4743c8bfbb395c60,0x8e87917f7672b8c0,0x1d0f22feece57181,0x3a1e45fdd9cae302,
+    0x743c8bfbb395c604,0xe87917f7672b8c08,0xd0f22feece571811,0xa1e45fdd9cae3023,0x43c8bfbb395c6047,0x87917f7672b8c08e,0x0f22feece571811d,0x1e45fdd9cae3023a
 };
 
 static const uint64_t vecC[64] = {
     0x3193c18562a02b4c,0x6327830ac5405698,0xc64f06158a80ad30,0x8c9e0c2b15015a61,0x193c18562a02b4c3,0x327830ac54056986,0x64f06158a80ad30c,0xc9e0c2b15015a618,
     0x93c18562a02b4c31,0x27830ac540569863,0x4f06158a80ad30c6,0x9e0c2b15015a618c,0x3c18562a02b4c319,0x7830ac5405698632,0xf06158a80ad30c64,0xe0c2b15015a618c9,
-    0xc18562a02b4c3193,0x830ac54056986327,0x6158a80ad30c64f,0xc2b15015a618c9e,0x18562a02b4c3193c,0x30ac540569863278,0x6158a80ad30c64f0,0xc2b15015a618c9e0,
-    0x8562a02b4c3193c1,0xac5405698632783,0x158a80ad30c64f06,0x2b15015a618c9e0c,0x562a02b4c3193c18,0xac54056986327830,0x58a80ad30c64f061,0xb15015a618c9e0c2,
+    0xc18562a02b4c3193,0x830ac54056986327,0x06158a80ad30c64f,0x0c2b15015a618c9e,0x18562a02b4c3193c,0x30ac540569863278,0x6158a80ad30c64f0,0xc2b15015a618c9e0,
+    0x8562a02b4c3193c1,0x0ac5405698632783,0x158a80ad30c64f06,0x2b15015a618c9e0c,0x562a02b4c3193c18,0xac54056986327830,0x58a80ad30c64f061,0xb15015a618c9e0c2,
     0x62a02b4c3193c185,0xc54056986327830a,0x8a80ad30c64f0615,0x15015a618c9e0c2b,0x2a02b4c3193c1856,0x54056986327830ac,0xa80ad30c64f06158,0x5015a618c9e0c2b1,
-    0xa02b4c3193c18562,0x4056986327830ac5,0x80ad30c64f06158a,0x15a618c9e0c2b15,0x2b4c3193c18562a,0x56986327830ac54,0xad30c64f06158a8,0x15a618c9e0c2b150,
+    0xa02b4c3193c18562,0x4056986327830ac5,0x80ad30c64f06158a,0x015a618c9e0c2b15,0x02b4c3193c18562a,0x056986327830ac54,0x0ad30c64f06158a8,0x15a618c9e0c2b150,
     0x2b4c3193c18562a0,0x56986327830ac540,0xad30c64f06158a80,0x5a618c9e0c2b1501,0xb4c3193c18562a02,0x6986327830ac5405,0xd30c64f06158a80a,0xa618c9e0c2b15015,
-    0x4c3193c18562a02b,0x986327830ac54056,0x30c64f06158a80ad,0x618c9e0c2b15015a,0xc3193c18562a02b4,0x86327830ac540569,0xc64f06158a80ad3,0x18c9e0c2b15015a6
+    0x4c3193c18562a02b,0x986327830ac54056,0x30c64f06158a80ad,0x618c9e0c2b15015a,0xc3193c18562a02b4,0x86327830ac540569,0x0c64f06158a80ad3,0x18c9e0c2b15015a6
 };
 
 static const uint64_t vecG[64] = {
-    0x20323ed082572324,0x40647da104ae4648,0x80c8fb42095c8c90,0x191f68412b91921,0x323ed0825723242,0x647da104ae46484,0xc8fb42095c8c908,0x191f68412b919210,
+    0x20323ed082572324,0x40647da104ae4648,0x80c8fb42095c8c90,0x0191f68412b91921,0x0323ed0825723242,0x0647da104ae46484,0x0c8fb42095c8c908,0x191f68412b919210,
     0x323ed08257232420,0x647da104ae464840,0xc8fb42095c8c9080,0x91f68412b9192101,0x23ed082572324203,0x47da104ae4648406,0x8fb42095c8c9080c,0x1f68412b91921019,
     0x3ed0825723242032,0x7da104ae46484064,0xfb42095c8c9080c8,0xf68412b919210191,0xed08257232420323,0xda104ae464840647,0xb42095c8c9080c8f,0x68412b919210191f,
-    0xd08257232420323e,0xa104ae464840647d,0x42095c8c9080c8fb,0x8412b919210191f6,0x8257232420323ed,0x104ae464840647da,0x2095c8c9080c8fb4,0x412b919210191f68,
-    0x8257232420323ed0,0x4ae464840647da1,0x95c8c9080c8fb42,0x12b919210191f684,0x257232420323ed08,0x4ae464840647da10,0x95c8c9080c8fb420,0x2b919210191f6841,
+    0xd08257232420323e,0xa104ae464840647d,0x42095c8c9080c8fb,0x8412b919210191f6,0x08257232420323ed,0x104ae464840647da,0x2095c8c9080c8fb4,0x412b919210191f68,
+    0x8257232420323ed0,0x04ae464840647da1,0x095c8c9080c8fb42,0x12b919210191f684,0x257232420323ed08,0x4ae464840647da10,0x95c8c9080c8fb420,0x2b919210191f6841,
     0x57232420323ed082,0xae464840647da104,0x5c8c9080c8fb4209,0xb919210191f68412,0x7232420323ed0825,0xe464840647da104a,0xc8c9080c8fb42095,0x919210191f68412b,
     0x232420323ed08257,0x464840647da104ae,0x8c9080c8fb42095c,0x19210191f68412b9,0x32420323ed082572,0x64840647da104ae4,0xc9080c8fb42095c8,0x9210191f68412b91,
-    0x2420323ed0825723,0x4840647da104ae46,0x9080c8fb42095c8c,0x210191f68412b919,0x420323ed08257232,0x840647da104ae464,0x80c8fb42095c8c9,0x10191f68412b9192
+    0x2420323ed0825723,0x4840647da104ae46,0x9080c8fb42095c8c,0x210191f68412b919,0x420323ed08257232,0x840647da104ae464,0x080c8fb42095c8c9,0x10191f68412b9192
 };
 
 static const uint64_t vecT[64] = {
@@ -72,14 +72,14 @@ static const uint64_t vecT[64] = {
 };
 
 static const uint64_t vecN[64] = {
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0
+    seedN,seedN,seedN,seedN,seedN,seedN,seedN,seedN,
+    seedN,seedN,seedN,seedN,seedN,seedN,seedN,seedN,
+    seedN,seedN,seedN,seedN,seedN,seedN,seedN,seedN,
+    seedN,seedN,seedN,seedN,seedN,seedN,seedN,seedN,
+    seedN,seedN,seedN,seedN,seedN,seedN,seedN,seedN,
+    seedN,seedN,seedN,seedN,seedN,seedN,seedN,seedN,
+    seedN,seedN,seedN,seedN,seedN,seedN,seedN,seedN,
+    seedN,seedN,seedN,seedN,seedN,seedN,seedN,seedN
 };
 
 static const uint64_t *msTab[256] = {
@@ -118,38 +118,38 @@ static const uint64_t *msTab[256] = {
 };
 
 static const uint64_t seedTab[256] = {
-    0, 0, 0, 0, 0, 0, 0, 0, // 0..7
-    0, 0, 0, 0, 0, 0, 0, 0, // 8..15
-    0, 0, 0, 0, 0, 0, 0, 0, // 16..23
-    0, 0, 0, 0, 0, 0, 0, 0, // 24..31
-    0, 0, 0, 0, 0, 0, 0, 0, // 32..39
-    0, 0, 0, 0, 0, seedT, 0, seedG, // 40..47
-    0, 0, 0, seedC, 0, 0, 0, 0, // 48..55
-    0, 0, 0, 0, 0, 0, 0, 0, // 56..63
-    seedA, seedA, 0, seedC, 0, 0, 0, seedG, // 64..71
-    0, 0, 0, 0, 0, seedT, 0, seedG, // 72..79
-    0, 0, 0, seedC, seedT, 0, 0, 0, // 80..87
-    0, 0, 0, 0, 0, 0, 0, 0, // 88..95
-    seedA, seedA, 0, seedC, 0, 0, 0, seedG, // 96..103
-    0, 0, 0, 0, 0, 0, 0, 0, // 104..111
-    0, 0, 0, 0, seedT, 0, 0, 0, // 112..119
-    0, 0, 0, 0, 0, 0, 0, 0, // 120..127
-    0, 0, 0, 0, 0, 0, 0, 0, // 128..135
-    0, 0, 0, 0, 0, 0, 0, 0, // 136..143
-    0, 0, 0, 0, 0, 0, 0, 0, // 144..151
-    0, 0, 0, 0, 0, 0, 0, 0, // 152..159
-    0, 0, 0, 0, 0, 0, 0, 0, // 160..167
-    0, 0, 0, 0, 0, 0, 0, 0, // 168..175
-    0, 0, 0, 0, 0, 0, 0, 0, // 176..183
-    0, 0, 0, 0, 0, 0, 0, 0, // 184..191
-    0, 0, 0, 0, 0, 0, 0, 0, // 192..199
-    0, 0, 0, 0, 0, 0, 0, 0, // 200..207
-    0, 0, 0, 0, 0, 0, 0, 0, // 208..215
-    0, 0, 0, 0, 0, 0, 0, 0, // 216..223
-    0, 0, 0, 0, 0, 0, 0, 0, // 224..231
-    0, 0, 0, 0, 0, 0, 0, 0, // 232..239
-    0, 0, 0, 0, 0, 0, 0, 0, // 240..247
-    0, 0, 0, 0, 0, 0, 0, 0  // 248..255
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 0..7
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 8..15
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 16..23
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 24..31
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 32..39
+  seedN, seedN, seedN, seedN, seedN, seedT, seedN, seedG, // 40..47
+  seedN, seedN, seedN, seedC, seedN, seedN, seedN, seedN, // 48..55
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 56..63
+  seedA, seedA, seedN, seedC, seedN, seedN, seedN, seedG, // 64..71
+  seedN, seedN, seedN, seedN, seedN, seedT, seedN, seedG, // 72..79
+  seedN, seedN, seedN, seedC, seedT, seedN, seedN, seedN, // 80..87
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 88..95
+  seedA, seedA,	seedN, seedC, seedN, seedN, seedN, seedG, // 96..103
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 104..111
+  seedN, seedN, seedN, seedN, seedT, seedN, seedN, seedN, // 112..119
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 120..127
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 128..135
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 136..143
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 144..151
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 152..159
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 160..167
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 168..175
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 176..183
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 184..191
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 192..199
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 200..207
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 208..215
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 216..223
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 224..231
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 232..239
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN, // 240..247
+  seedN, seedN, seedN, seedN, seedN, seedN, seedN, seedN  // 248..255
 };
 
 // rotate "v" to the left by "s" positions
@@ -196,6 +196,11 @@ inline uint64_t NT64(const char * kmerSeq, const unsigned k, const unsigned seed
     hVal *= seed ^ k * multiSeed;
     hVal ^= hVal >> multiShift;
     return hVal;
+}
+
+// canonical ntHash, base function
+inline uint64_t NTC64(const char * kmerSeq, const unsigned k) {
+    return (getFhval(kmerSeq, k) ^ getRhval(kmerSeq, k));
 }
 
 // canonical ntHash
@@ -300,6 +305,28 @@ void NTM64(const char * kmerSeq, const unsigned k, const unsigned m, uint64_t *h
 void NTM64(const unsigned char charOut, const unsigned char charIn, const unsigned k, const unsigned m, uint64_t *hVal) {
     uint64_t bVal=0, tVal=0;
     hVal[0] = bVal = rol(hVal[0], 1) ^ msTab[charOut][k%64] ^ msTab[charIn][0];
+    for(unsigned i=1; i<m; i++) {
+        tVal = bVal * (i ^ k * multiSeed);
+        tVal ^= tVal >> multiShift;
+        hVal[i] =  tVal;
+    }
+}
+
+// canonical multi-hash version of ntHash
+void NTMC64(const char * kmerSeq, const unsigned k, const unsigned m, uint64_t *hVal, uint64_t& fhVal, uint64_t& rhVal) {
+    uint64_t bVal=0, tVal=0;
+    hVal[0] = bVal = NTPC64(kmerSeq, k, fhVal, rhVal);
+    for(unsigned i=1; i<m; i++) {
+        tVal = bVal * (i ^ k * multiSeed);
+        tVal ^= tVal >> multiShift;
+        hVal[i] =  tVal;
+    }
+}
+
+// canonical multi-hash version of ntHash for sliding k-mers
+void NTMC64(uint64_t& fhVal, uint64_t& rhVal, const unsigned char charOut, const unsigned char charIn, const unsigned k, const unsigned m, uint64_t *hVal) {
+    uint64_t bVal=0, tVal=0;
+    hVal[0] = bVal = NTPC64(fhVal, rhVal, charOut, charIn, k);
     for(unsigned i=1; i<m; i++) {
         tVal = bVal * (i ^ k * multiSeed);
         tVal ^= tVal >> multiShift;
