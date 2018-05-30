00288000d9f731d517b11de86382db6979896e67
mohamadi <mohamadi.hamid@gmail.com>
2018-05-30 10:42:39 -0700

    sttest modified

--- sttest.cpp
+++ sttest.cpp
@@ -1,38 +1,52 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include "stHashIterator.hpp"
-
 using namespace std;
 
-int main()
+int main(int argc, const char *argv[])
 {
+    size_t countOnes=0;
+    
+    std::vector<std::string> seedString;
+    seedString.push_back("110001100111000001110100110110100010011101");
+    seedString.push_back("000001111100101110111000001011010100011110");
+    seedString.push_back("011110001010110100000111011101001111100000");
+    seedString.push_back("101110010001011011001011100000111001100011");
+    
+    
     /* test sequence */
-    std::string seq = "GAGTGTCAAACATTCAGACAACAGCAGGGGTGCTCTGGAATCCTATGTGAGGAACAAACATTCAGGCCACAGTAG";
+    //std::string seq = "GAGTGTCAAACATTCAGACAACAGCAGGGGTGCTCTGGAATCCTATGTGAGGAACAAACATTCAGGCCACAGTAG";
     
-   
-    /* h is the number of hashes for each k-mer */
-    unsigned h = 4;
+    /* h is the number of spaced seeds and k is the length of spaced seeds */
+    unsigned h, k;
+    h = seedString.size();
+    k = seedString[0].size();
     
-    std::vector<std::string> seedSeq(h);
-    seedSeq[0]="110001100111000001110100110110100010011101";
-    seedSeq[1]="000001111100101110111000001011010100011110";
-    seedSeq[2]="011110001010110100000111011101001111100000";
-    seedSeq[3]="101110010001011011001011100000111001100011";
+    std::vector<std::vector<unsigned> > seedSet = parseSeed(seedString);
     
-    //cerr << seedSeq[3] << endl;
-//    ,"110011110011101","111110110011101","100111110011101"};
-    
-    stHashIterator ssitr(seq, seedSeq, h, seedSeq[0].size());
-    
-    while (ssitr != ssitr.end()) {
-        std::cout << (*ssitr)[0] << "\t" << (*ssitr)[1] <<"\t" << (*ssitr)[2] << "\t" <<(*ssitr)[3] <<"\t" << std::endl;
-        std::cout << (ssitr.strand())[0] << "\t" << (ssitr.strand())[1] <<"\t" << (ssitr.strand())[2] << "\t" <<(ssitr.strand())[3] <<"\t" << std::endl;
-
-        ++ssitr;
+    clock_t sTime = clock();
+    std::ifstream in(argv[argc-1]);
+    bool good = true;
+    for(string seq, hseq; good;) {
+        good = static_cast<bool>(getline(in, hseq));
+        good = static_cast<bool>(getline(in, seq));
+        good = static_cast<bool>(getline(in, hseq));
+        good = static_cast<bool>(getline(in, hseq));
+        if(good){
+            stHashIterator ssitr(seq, seedSet, h, k);
+            while (ssitr != ssitr.end()) {
+                if((ssitr.strand())[0])
+                    ++countOnes;
+                ++ssitr;
+            }
+        }
+        
     }
-    
-    std::cout << endl << endl;
+
+    //std::cerr<< countOnes << std::endl;
+    std::cerr << (double)(clock() - sTime)/CLOCKS_PER_SEC << "\n";
     
     
     return 0;
