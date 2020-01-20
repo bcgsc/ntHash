25a605c62131265a1522e4a17f3334f690b96857
Johnathan Wong <jowong@hpce704.hpc.bcgsc.ca>
2020-01-20 09:32:50 -0800

    clang-formatted files

--- sstest.cpp
+++ sstest.cpp
@@ -1,28 +1,29 @@
+#include "ssHashIterator.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
-#include "ssHashIterator.hpp"
 
 using namespace std;
 
-int main()
+int
+main()
 {
-    /* test sequence */
-    std::string seq = "GAGTGTCAAACATTCAGACAACAGCAGGGGTGCTCTGGAATCCTATGTGAGGAACAAACATTCAGGCCACAGTAG";
+	/* test sequence */
+	std::string seq = "GAGTGTCAAACATTCAGACAACAGCAGGGGTGCTCTGGAATCCTATGTGAGGAACAAACATTCAGGCCACAGTAG";
 
-    /* seed is the spaced seed */
-    std::string seedStr("110001100111000001110100110110100010011101");
+	/* seed is the spaced seed */
+	std::string seedStr("110001100111000001110100110110100010011101");
 
-    std::vector<bool> seed;
-    for(auto p : seedStr)
-        seed.push_back(p=='1');
+	std::vector<bool> seed;
+	for (auto p : seedStr)
+		seed.push_back(p == '1');
 
-    ssHashIterator ssitr(seq, seed, seed.size());
+	ssHashIterator ssitr(seq, seed, seed.size());
 
-    while (ssitr != ssitr.end()) {
-        std::cout << *ssitr << std::endl;
-        ++ssitr;
-    }
+	while (ssitr != ssitr.end()) {
+		std::cout << *ssitr << std::endl;
+		++ssitr;
+	}
 
-    return 0;
+	return 0;
 }
